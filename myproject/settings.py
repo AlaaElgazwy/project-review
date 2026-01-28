818f3daa7d6ae84c090daac4986f86b7f018efab
Alaa Elgazwy <ai6949347@gmail.com>
2026-01-28 11:54:22 +0200

    fix csrf trusted origins

--- myproject/settings.py
+++ myproject/settings.py
@@ -116,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-calculator-latest.onrender.com',
+]
